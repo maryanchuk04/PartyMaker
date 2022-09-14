621e1af8b6f1eaba3fb9b6fd4ab52e2b77a81a5a
MaxChabanenko <chabanenko@knu.ua>
2022-09-14 14:06:11 +0300

    Supplier login page

---
+++ PartyMaker.Web.Client/ClientApp/src/services/SupplierService.js
@@ -0,0 +1,13 @@
+﻿export class SupplierService {
+    url = "https://localhost:7118/api/Supplier"
+
+    async insertNewSupplier(data) {
+        return await fetch(this.url + `/AddNewSupplier`, {
+            method: "post",
+            headers: new Headers({
+                'Content-Type': 'application/json',
+            }),
+            body: JSON.stringify(data)
+        });
+    }
+}
