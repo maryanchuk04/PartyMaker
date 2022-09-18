290a08d7471ce6677b72fa0d442aa2ade10f32d8
Maks <lion20914king@gmail.com>
2022-09-18 12:30:44 +0300

    Responsive header

--- PartyMaker.Web.Client/ClientApp/src/components/NavMenu.js
+++ PartyMaker.Web.Client/ClientApp/src/components/NavMenu.js
@@ -1,52 +1,80 @@
-import React, { Component } from 'react';
-import {  Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+import {  Container, Navbar, NavbarBrand, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 import { isAuth } from '../utils/helpers';
+import useMedia from 'use-media';
 
-export class NavMenu extends Component {
-  constructor (props) {
-    super(props);
+export function NavMenu(){
+  const [state, setState] = useState({
+    userAuth : false
+  });
+  const [open,setOpen] = useState(false);
+  const media = useMedia({ maxWidth : 590});
 
-    this.state = {
-      userAuth : false
-    };
-  }
 
-  componentDidMount(){
+  useEffect(()=>{
     if(isAuth()){
-      this.setState({
+      setState({
         userAuth : true
       });
     }
-  }
+  },[])
 
-
-    render() {
-        return (
-            <header>
-                <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white  box-shadow h-100" >
-                    <Container>
-                        <NavbarBrand tag={Link} to="/"><h1>Party<span style ={{color : "#1aa94b"}}>Maker</span></h1></NavbarBrand>
-
-                        <ul className="navbar-nav">
-                            <NavItem style = {{display : "contents"}}>
-                                <NavLink tag={Link} className="text-light m-1" to="/">Home</NavLink>
-                            </NavItem>
-                            <NavItem style = {{display : "contents"}}>
-                                <NavLink tag={Link} className="text-light m-1" to="/сontact">Contact Us</NavLink>
-                            </NavItem>
-                            <NavItem style = {{display : "contents"}}>
-                                <NavLink tag={Link} className="text-light sing-in m-1" to="/auth/login">Sing In</NavLink>
-
-                            </NavItem>
-                            <NavItem style = {{display : "contents"}}>
-                                <NavLink tag={Link} className="text-light  create-account-li ms-1" to="/create-account">Sign Up</NavLink>
-                            </NavItem>
-                        </ul>
-                    </Container>
-                </Navbar>
-            </header >
-        );
+  const handleMenuClick =()=>{  
+    if(open){
+        setOpen(false);
     }
+    else setOpen(true);
+}
+
+  return !media ?<>
+        <header>
+          <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white  box-shadow h-100 " >
+              <Container>
+                  <NavbarBrand tag={Link} to="/"><h1>Party<span style ={{color : "#1aa94b"}}>Maker</span></h1></NavbarBrand>
+                    <ul className="navbar-nav flex-grow">
+                        <NavItem style = {{display : "contents"}}>
+                            <NavLink tag={Link} className="text-light m-1" to="/">Home</NavLink>
+                        </NavItem>
+                        <NavItem style = {{display : "contents"}}>
+                            <NavLink tag={Link} className="text-light m-1" to="/сontact">Contact Us</NavLink>
+                        </NavItem>
+                        <NavItem style = {{display : "contents"}}>
+                            <NavLink tag={Link} className="text-light sing-in m-1" to="/auth/login">Sing In</NavLink>
+                        </NavItem>
+                        <NavItem style = {{display : "contents"}}>
+                            <NavLink tag={Link} className="text-light  create-account-li ms-1" to="/create-account">Sign Up</NavLink>
+                        </NavItem>
+                    </ul>
+              </Container>
+          </Navbar>
+      </header ></> :
+      <>
+        <header className ='responsive-header container'>
+          <NavbarBrand tag={Link} to="/"><h1>Party<span style ={{color : "#1aa94b"}}>Maker</span></h1></NavbarBrand>
+          <div className="responsive_menu">
+            <div className={`btnn ${open ? 'active' : 'not-active'}`} onClick ={handleMenuClick} id = 'btn'>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+          </div>
+          {open ? <div className="dropdown-menu-block container">
+                    <NavItem style = {{display : "contents"}}>
+                        <NavLink tag={Link} className="text-light m-1" to="/" onClick = {()=>setOpen(false)}>Home</NavLink>
+                    </NavItem>
+                    <NavItem style = {{display : "contents"}}>
+                        <NavLink tag={Link} className="text-light m-1" to="/сontact" onClick = {()=>setOpen(false)}>Contact Us</NavLink>
+                    </NavItem>
+                    <NavItem style = {{display : "contents"}}>
+                        <NavLink tag={Link} className="text-light sing-in m-1" to="/auth/login" onClick = {()=>setOpen(false)}>Sing In</NavLink>
+                    </NavItem>
+                    <NavItem style = {{display : "contents"}}>
+                        <NavLink tag={Link} className="text-light  create-account-li ms-1" to="/create-account" onClick = {()=>setOpen(false)}>Sign Up</NavLink>
+                    </NavItem>
+                  </div>  : null
+          } 
+        </header>
+      </>
 }
